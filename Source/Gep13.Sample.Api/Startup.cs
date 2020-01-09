02067670c26d06d1a8b98b1db8a02e8e54ac2feb
jatin-lab49 <jatin.thakkar@lab49.com>
2020-01-09 22:06:40 +0000

    [CHORE]: Had to updated Startup.cs to match integrations tests
    Looks like global.asax.cs is no longer used, but the code was never updated
    The changes allow the application to actually run as expected

--- Source/Gep13.Sample.Api/Startup.cs
+++ Source/Gep13.Sample.Api/Startup.cs
@@ -7,7 +7,14 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System.Reflection;
+using System.Web.Http;
+
+using Autofac.Integration.WebApi;
+using Gep13.Sample.Api.Mappers;
+using Gep13.Sample.Common;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 
 [assembly: OwinStartup(typeof(Gep13.Sample.Api.Startup))]
@@ -22,6 +29,35 @@ namespace Gep13.Sample.Api
         public static void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            var config = new HttpConfiguration();
+            config.EnableCors();
+
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            //// config.Filters.Add(new AuthorizeAttribute());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+
+            var containerBuilder = AutofacBootstrapper.Configure();
+
+            containerBuilder.RegisterApiControllers(Assembly.Load("Gep13.Sample.Api"));
+
+            var container = containerBuilder.Build();
+
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            AutoMapperConfiguration.Configure();
+
+            app.UseAutofacMiddleware(container);
+
+            app.UseWebApi(config);
         }
     }
 }
